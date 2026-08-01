f9ef723fd5158d15e84e3dba8e0d8318d3f4dfbc
Bryce Drynan <bryce@drynan.com>
2026-08-01 13:07:36 -0400

    Move DB credentials out of source into environment variables

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float, Boolean
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
 
 
-
-DATABASE_URL = "postgresql://apiuser:apipass@db:5432/apidb"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
